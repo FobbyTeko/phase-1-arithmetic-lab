220e17501b807578779dea5eb429a5cc01d5396f
Tiri Tuatai <tiri.jnr@gmail.com>
2024-02-16 14:47:46 +0800

    Arithmetic Lab done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1 * num2)
 
+const random = 123456789
+Number.isInteger(random)
+    
+let num3 =16;
+let num4 = 6;
+const mod = (num3 % num4)
+
+const max = Math.max(20)
 // Write your code here
